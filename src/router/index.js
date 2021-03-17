ec14c38363b156eb421b4ed93089cc6292fb9342
Bulb Yu <bulb.yu@neutec.com.tw>
2021-03-17 17:51:35 +0800

    add sample2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Sample from '../views/sample2.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,15 +11,13 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/sample2',
+        name: 'sample2',
+        // component: () => import(/* webpackChunkName: "sample2" */ '../views/sample2.vue')
+        component: Sample
     }
-    // {
-    //     path: '/about',
-    //     name: 'About',
-    //     // route level code-splitting
-    //     // this generates a separate chunk (about.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/sample2.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+      <div ref="sample2" />
+  </div>
+</template>
+
+<script>
+import * as PIXI from 'pixi.js'
+// import bunny from '@/assets/bunny.png'
+
+export default {
+    name: "sample2",
+    data () {
+        return {
+            app: null
+        }
+    },
+    mounted () {
+        this.init()
+    },
+    methods: {
+        init () {
+            // 讓chrome pixi devTool出現
+            if (process.env.NODE_ENV === 'development') {
+                window.PIXI = PIXI
+            }
+
+            this.app = new PIXI.Application({
+                width: 800,
+                height: 600
+                // transparent: true // 背景透明
+            })
+
+            // 展開全螢幕
+            this.app.renderer.view.style.position = 'absolute'
+            this.app.renderer.view.style.display = 'block'
+            this.app.renderer.autoResize = true
+            this.app.renderer.resize(window.innerWidth, window.innerHeight)
+
+            this.$refs.sample2.appendChild(this.app.view)
+
+            // this.app.loader.add('bunny', logo)
+
+            let triangle = new PIXI.Graphics();
+
+            triangle.beginFill(0x006DDD)
+            triangle.moveTo(0,0)
+            triangle.lineTo(0, 300);
+            triangle.lineTo(150, 300);
+            triangle.lineTo(0, 0);
+            triangle.endFill();
+            this.app.stage.addChild(triangle)
+
+
+            // 單向漸層
+            const sprite1 = new PIXI.Sprite(this.gradientBg({
+                colorArr: ['#fff','#CB2929','#800505', '#000'],
+                // stepArr: [0, 0.2, 0.8, 1],
+                width: 34,
+                height: 67
+            }))
+
+
+            this.app.stage.addChild(sprite1)
+        },
+        /**
+         * 繪製單向漸層
+         * @param {Array} param.colorArr required, 漸層顏色
+         * @param {Array} param.stepArr optional, 漸層自定義step, 未提供使用平均區間
+         * @param {Number} param.width required, 寬度
+         * @param {Number} param.height required, 高度
+         * @returns PIXI.Texture
+         */
+        gradientBg({colorArr, stepArr = [], width, height}){
+            const canvas = document.createElement("canvas");
+            canvas.setAttribute("width", width);
+            canvas.setAttribute("height", height);
+
+            const ctx = canvas.getContext("2d");
+            const grd = ctx.createLinearGradient(0, 0, 0, height);
+
+            colorArr.forEach((rawColor, idx) => {
+                const step = stepArr[idx] || (1 / (colorArr.length -1) ) * idx
+                console.log(step)
+                grd.addColorStop(step, rawColor); 
+            })
+
+            ctx.fillStyle = grd;
+            ctx.fillRect(0, 0, width, height);
+            return new PIXI.Texture.from(canvas);
+        },       
+    }
+}
+</script>
