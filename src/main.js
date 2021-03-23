d51359b916fd656c32b4ea2c8e61400f74926470
Bulb Yu <bulb.yu@neutec.com.tw>
2021-03-23 12:15:06 +0800

    include GSAP

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import * as PIXI from 'pixi.js'
+import { gsap } from 'gsap'
+import { PixiPlugin } from 'gsap/PixiPlugin'
+
 import '@/css/index.scss'
+
+// register the plugin
+gsap.registerPlugin(PixiPlugin)
+
+// give the plugin a reference to the PIXI object
+PixiPlugin.registerPIXI(PIXI)
 
 Vue.config.productionTip = false
 
